5c8c42f2d2a80a7fcddd216add8bccb761985e40
Haouo <q18131500@gs.ncku.edu.tw>
2025-11-21 00:23:48 +0800

    Initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,143 @@
+{ config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "haouo";
+  home.homeDirectory = "/Users/haouo";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+    neofetch
+    zip xz unzip
+    ripgrep eza fzf yazi
+    mtr dnsutils
+    which file cowsay tree gnused gnutar gawk zstd gnupg
+    btop 
+    git
+    wget curl
+    gnumake cmake meson ninja
+    vim zellij lazygit glow gnuplot
+    texliveSmall texlab bibtex-tidy
+    typst tinymist typstyle
+    age sops
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/haouo/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    settings.user.name = "Haouo";
+    settings.user.email = "q18131500@gs.ncku.edu.tw";
+  };
+
+  programs.direnv = {
+    enable = true;
+  };
+
+  programs.nushell = {
+    enable = true;
+    shellAliases = {
+      vi = "nvim";
+      vim = "nvim";
+      ll = "ls -al";
+      update = "home-manager switch";
+    };
+  };
+
+  programs.carapace = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  programs.starship = {
+    settings = {
+      add_newline = true;
+      character  = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
+  };
+
+  programs.nixvim = {
+    enable = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+    colorschemes.base16 = {
+      enable = true;
+      colorscheme = "0x96f";
+    };
+  };
+
+  sops = {  
+    defaultSopsFile = ./secrets/ssh.yaml;
+    age.keyFile = "/Users/haouo/.config/sops/age/keys.txt";
+    secrets.ssh_privkey = {};
+  };
+}
